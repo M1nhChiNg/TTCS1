f63592a53b174fd5aede4ee84d8f31ccf30615e2
namvuj7-glitch <namvuj7@gmail.com>
2025-11-05 09:31:49 +0700

    Add new routes for Login, Find, and StoryReader

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Home from "./component/Home";
 import Detail from "./component/Detail";
+import Login from "./component/Login";
+import Find from "./component/Find"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StoryReader from "./component/StoryReader";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/comics/:slug" element={<Detail></Detail>}></Route>
+        <Route path="/comics/:StoryID" element={<Detail></Detail>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/find" element={<Find></Find>}></Route>
+        <Route path="/read/:ChapterID" element={<StoryReader></StoryReader>}></Route>
       </Routes>
     </Router>
   );
